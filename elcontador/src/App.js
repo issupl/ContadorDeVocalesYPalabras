38bed6a5b3d6a9a7e9e901c89708dd245a7f9ce7
Israel <issupl@protonmail.com>
2022-03-30 13:48:32 +0200

    Idento el App

--- elcontador/src/App.js
+++ elcontador/src/App.js
@@ -2,21 +2,21 @@ import './App.css';
 import React,{useState,useEffect} from 'react';
 import { entradaDeTexto } from './hepers/helpers';
 function App() {
-const [salida,setSalida] = useState({});
+  const [salida,setSalida] = useState({});
 
-function groupWords(event){
- setSalida(entradaDeTexto(event.target.value))
-}
+  function groupWords(event){
+  setSalida(entradaDeTexto(event.target.value))
+  }
 
-useEffect(()=>{
- 
-},[salida.numPalabras,salida.numVocales])
+  useEffect(()=>{
+  
+  },[salida.numPalabras,salida.numVocales])
 
-  return (
-    <>Número de vocales {salida.numVocales} , número de palabras {salida.numPalabras}
-    <textarea name="textarea" rows="10" cols="50" onChange={(event)=>{groupWords(event)}}/>
-    </>
-  )
+    return (
+      <>Número de vocales {salida.numVocales} , número de palabras {salida.numPalabras}
+      <textarea name="textarea" rows="10" cols="50" onChange={(event)=>{groupWords(event)}}/>
+      </>
+    )
 }
 
 export default App;
